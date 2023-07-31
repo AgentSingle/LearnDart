5e6c03878ff74724aae0b263b29917d6340965c6
unknown <ataota.org@gmail.com>
2023-07-31 14:46:56 +0530

    VERSION:1.0.0, First Dart Code

---
+++ DartBesic/dart_datatype.dart
@@ -0,0 +1,34 @@
+void main () {
+  print("This is Data type tutorial");
+
+// INTIGER AND FLOATING POINT NUMBER [DOUBLE]
+  int isIntiger = 12;
+  double isDouble = 12.5;
+  print("Intiger: ${isIntiger}, Double(floating number): ${isDouble}");
+
+// STRING AND NUMBER STRICTLY DEFINE
+  String myname = "Sudipta Mandal";
+  num itIsANumber = 50;
+  print("String: ${myname}, Number: ${itIsANumber}");
+
+// VARIABLE AND CONSTANT
+  var name = "Sudipta Mandal";
+  const number = 10;
+  /* TRY BELOW CODE [ TEST THE OUTPUT ] */ 
+  // name = "soumen jana";
+  // number = 25;
+  print("Variable: ${name} & const: ${number}");
+
+// UNDER-STANDING DYNAMIC DATA TYPE 
+  dynamic x = "Tom Crush";
+  /* TRY BELOW CODE [ TEST THE OUTPUT ] */ 
+  // x = "Nabin Kumar";
+  // x = [1, 2, 3, 4];
+  print(x);
+
+// UNDERSTANDING FINAL IN DART
+  final v1 = 12;
+  /* TRY BELOW CODE [ TEST THE OUTPUT ] */ 
+  // v1 = 15;
+  print(v1);
+}
