c6b16ca457d65f845e768f804a99365b51a34cfa
unknown <ataota.org@gmail.com>
2023-08-09 21:19:40 +0530

    VERSION-1.0.6, Boolean

---
+++ DartBesic/d7_Boolean.dart
@@ -0,0 +1,12 @@
+// Unlike JavaScript, the Boolean data type recognizes only the literal true as true. Any other value is considered as false
+// void main(){
+//   var str ="Nabin Reddy";
+
+//   if(str){
+//     print('String is not empty'); 
+//   } else {
+//     print('Empty String'); 
+//   }
+// }
+
+// THIS CODE WILL RETURN AN ERROR
