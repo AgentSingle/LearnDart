863e9e5444261f91632ba799a6fc675e0688395f
unknown <ataota.org@gmail.com>
2023-08-10 20:27:38 +0530

    VERSION-1.0.7, Dart List

---
+++ DartBesic/d8_list.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  var num_list = [1,2,3]; 
+  print(num_list); 
+
+  var lst = []; 
+  lst.add(12); 
+  lst.add(13); 
+  print(lst); 
+
+  print(lst.first);
+  print(lst.last);
+  print(lst.isEmpty); 
+  print(lst.length); 
+  print(lst.reversed); 
+  // print(lst.single); // IT WILL THROW AN ERROR
+}
